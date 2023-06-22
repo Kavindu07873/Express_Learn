04b8ba7bce523d617ff913199661c885c341f26f
Kavindu07873 <Dilshankavindu07715@gmail.com>
2023-06-22 15:06:36 +0530

    params set /:id

--- app.js
+++ app.js
@@ -12,13 +12,20 @@ app.get('/',(req,res)=>{
     // console.log('get reques has come')
 })
 
-
+//json Object eka gaththa
 app.get('/Customer',(req,res)=>{
     res.send('Hello Customer')
     console.log(req.body)
 })
 
-
+app.get('/Customer/:id',(req,res)=>{
+    console.log(req.params)
+    res.send(' Customer parem id Come')
+})
+app.get('/Customer/',(req,res)=>{
+    console.log(req.params)
+    res.send(' Customer parem Name Come')
+})
 app.listen(port,(req ,res)=>{
     console.log(`Express App listen ${port}`);
 })
